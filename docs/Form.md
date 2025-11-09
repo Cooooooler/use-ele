6ccc454413adc9456b5dea996db64d4db3c3a54f
Lumengling <3422056558@qq.com>
2025-11-09 22:37:12 +0800

    feat: fieldMappingTime markdown

--- docs/Form.md
+++ docs/Form.md
@@ -47,10 +47,32 @@ description="类型为
 `{showCollapseButton: boolean; collapsed: boolean; collapsedRows: number; collapseTriggerResize: boolean;}`">
 </code>
 
-### 紧凑模式
+## 紧凑模式
 
 <code
 src="./demo/form/Compact.vue"
 title="compact"
 description="紧凑模式">
+</code>
+
+## 表单字段映射到时间格式
+
+:::info{title=表单字段映射到时间格式}
+使用场景：在某些场景下，需要将表单字段映射到时间格式，比如：`[startDate, endDate]`映射为时间范围选择器。
+
+使用方法：
+
+` [
+  string,
+  [string, string],
+  (((value: any, fieldName: string) => any) | [string, string] | null | string)?
+][]`
+
+第一个元素为需要转换的字段名称，第二个元素为转换的目标字段名称，第三个元素为可选的格式化函数或映射关系，默认为 `'YYYY-MM-DD'`。
+:::
+
+<code
+src="./demo/form/FieldMappingTime.vue"
+title="FieldMapping"
+description="表单字段映射到时间格式">
 </code>
---
+++ docs/demo/form/FieldMappingTime.vue
@@ -0,0 +1,30 @@
+<template>
+  <Form />
+</template>
+
+<script setup lang="ts">
+import { useEleForm } from '../adapter';
+import { ElMessage } from 'element-plus';
+
+const [Form] = useEleForm({
+  handleSubmit: (values: Record<string, any>) => {
+    ElMessage.success(`表单数据：${JSON.stringify(values)}`);
+    console.log(values);
+  },
+  fieldMappingTime: [['date', ['startDate', 'endDate']]],
+  schema: [
+    {
+      component: 'DatePicker',
+      fieldName: 'date',
+      label: '时间范围',
+      componentProps: {
+        type: 'daterange',
+        startPlaceholder: '开始日期',
+        endPlaceholder: '结束日期',
+        format: 'YYYY-MM-DD',
+        valueFormat: 'YYYY-MM-DD',
+      },
+    },
+  ],
+});
+</script>
